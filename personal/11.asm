098be0c11bb4a457743e68dd795679ba544776bb
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2024-11-01 10:43:51 -0400

    replace mul for imul

--- personal/11.asm
+++ personal/11.asm
@@ -1,11 +1,11 @@
 ; use mul (unsigned number)
 extern printf
 section .data
-    n1 dd 4294967295
-	n2 dd 2 
-    ; lu (unsigned number of 32 bits) 
-    ; llu(unsigned number of 64 bits) 
-	fmt db "%u x %u = %llu", 0
+    n1 dd -2147483648 
+	n2 dd  2147483647
+    ; d = ld = 4 bytes (unsigned number of 32 bits) 
+    ; lld = 8 bytes (unsigned number of 64 bits) 
+	fmt db "%ld x %ld = %lld", 0
 section .bss
 section .text
 	global main:
@@ -18,20 +18,18 @@ main:
     xor r11,r11
 
 	mov eax, [n1]
-	mul dword [n2]
+	imul dword [n2]
 
 	limpiar:
 	sub rsp, 32 
  	mov rcx, fmt
 
-    mov r10, rdx 
-    mov r11, rax
+    mov r9, rdx 
  	mov edx, [n1]
  	mov r8d, [n2]
 
-    shl r10, 32
-    or r10,  r11; problema el valor de r10 se borra 
- 	mov r9, r10
+    shl r9, 32
+    or r9,  rax
 	call printf
 	add rsp, 32 
 	ret 
